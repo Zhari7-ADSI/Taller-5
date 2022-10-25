267fbf72b6868b3dc7805ffb57f5bf2919a6a926
Zhari7 <meneseszharick29@gmail.com>
2022-10-25 12:12:25 -0500

    Ejercicio 2

--- main.js
+++ main.js
@@ -1,21 +1,41 @@
 addEventListener("DOMContentLoaded", (e) => {
 
     let form = document.querySelector('#form');
-    let sum = 0;
+    let res = document.querySelector("#resultado");
+    let sum = 0, desc;
+    let info = document.querySelector("#info");
 
+    document.querySelector("#dato").readOnly = false;
+    document.querySelector("#boton").disabled = false;
+
+    let habilitar = () => {
+        info.style.display = "inline-block";
+        document.querySelector("#dato").readOnly = true;
+        document.querySelector("#boton").disabled = true;
+    }
+    let descuento = () => {
+        if (sum > 10000) {
+            desc = sum - (sum * 0.10);
+            res.insertAdjacentHTML("beforeend", `<h3> 10% de descuento, Total a pagar: $${desc}<h3/>`);
+        }
+        else {
+            res.insertAdjacentHTML("beforeend", `<h3>Total a pagar: $${sum}<h3/>`);
+        }
+
+    }
     form.addEventListener("submit", (e) => {
         e.preventDefault();
+        let precio = parseInt(document.querySelector("#dato").value);
+        form.reset();
 
-        let dato = parseInt(document.querySelector('#dato').value);
-
-        if (dato > 0) {
-            sum += dato;
+        if (precio > 0) {
+            sum += precio;
         }
         else {
             alert('Dale al botón de "Aceptar" 🡖');
-            document.querySelector('#resultado').insertAdjacentHTML("beforeend", sum);
+            habilitar();
+            descuento();
         }
-        form.reset();
     })
 
 })
